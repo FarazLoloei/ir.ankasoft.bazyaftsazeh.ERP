0d8cee8c7ec43f40bd14a05fdedc647257f2ec44
Faraz.Loloei <FarazLoloei@gmail.com>
2017-09-26 14:08:53 +0330

    infrastructure project was added

---
+++ ir.ankastore.infrastructure/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ir.ankasoft.infrastructure
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        /// <summary>
+        /// Commits the changes to the underlying data store.
+        /// </summary>
+        /// <param name="resetAfterCommit">When true, all the previously retrieved objects should be cleared from the underlying model / cache.</param>
+        void Commit(bool resetAfterCommit);
+
+        /// <summary>
+        /// Undoes all changes to the entities in the model.
+        /// </summary>
+        void Undo();
+    }
+}
---
+++ ir.ankastore.infrastructure/IUnitOfWorkFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace ir.ankasoft.infrastructure
+{
+    public interface IUnitOfWorkFactory
+    {
+        /// <summary>
+        /// Creates a new instance of a unit of work
+        /// </summary>
+        IUnitOfWork Create();
+
+        /// <summary>
+        /// Creates a new instance of a unit of work
+        /// </summary>
+        /// <param name="forceNew">When true, clears out any existing in-memory data store / cache first.</param>
+        IUnitOfWork Create(bool forceNew);
+    }
+}
