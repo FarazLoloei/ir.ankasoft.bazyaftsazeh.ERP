6df1a9c837f8261a68bec17f552ab17791a00d83
Faraz.Loloei <FarazLoloei@gmail.com>
2017-09-26 17:15:54 +0330

    Organization and Importer Objects added

---
+++ ir.ankasoft.bazyaftsazeh.ERP.entities/Importer.cs
@@ -0,0 +1,76 @@
+﻿using ir.ankasoft.entities;
+using ir.ankasoft.infrastructure;
+using ir.ankasoft.resource;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ir.ankasoft.bazyafsazeh.ERP.entities
+{
+    public class Importer : DomainEntity<long>, IDateTracking, IUserTracking
+    {
+        public long PartyRefRecId { get; set; }
+
+        [ForeignKey(nameof(PartyRefRecId))]
+        public Party Party { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Family { get; set; }
+
+        [MaxLength(20)]
+        public string ImporterNumber { get; set; }
+
+        public ICollection<PostalAddress> PostalAddressCollection { get; set; }
+
+        public ICollection<Communication> CommunicationCollection { get; set; }
+
+        #region IDateTracking
+
+        public DateTime createdDateTime { get; set; }
+
+        public DateTime? modifiedDateTime { get; set; }
+
+        #endregion IDateTracking
+
+        #region IUserTracking
+
+        public long creatorUserRefRecId { get; set; }
+
+        [ForeignKey(nameof(creatorUserRefRecId))]
+        public ApplicationUser creatorUser { get; set; }
+
+        public long? modifierUserRefRecId { get; set; }
+
+        [ForeignKey(nameof(modifierUserRefRecId))]
+        public ApplicationUser modifierUser { get; set; }
+
+        #endregion IUserTracking
+
+        #region Validation
+
+        public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                yield return new ValidationResult(string.Format(Resource._0CanntBeEmpty, nameof(Name)), new[] { nameof(Name) });
+            }
+            if (string.IsNullOrEmpty(Family))
+            {
+                yield return new ValidationResult(string.Format(Resource._0CanntBeEmpty, nameof(Family)), new[] { nameof(Family) });
+            }
+
+            if (string.IsNullOrEmpty(ImporterNumber))
+            {
+                yield return new ValidationResult(string.Format(Resource._0CanntBeEmpty, nameof(ImporterNumber)), new[] { nameof(ImporterNumber) });
+            }
+        }
+
+        #endregion Validation
+    }
+}
---
+++ ir.ankasoft.bazyaftsazeh.ERP.entities/Organization.cs
@@ -0,0 +1,73 @@
+﻿using ir.ankasoft.entities;
+using ir.ankasoft.infrastructure;
+using ir.ankasoft.resource;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ir.ankasoft.bazyafsazeh.ERP.entities
+{
+    public class Organization : DomainEntity<long>, IDateTracking, IUserTracking
+    {
+        public long PartyRefRecId { get; set; }
+
+        [ForeignKey(nameof(PartyRefRecId))]
+        public Party Party { get; set; }
+
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string RegisterationNumber { get; set; }
+
+        public string RegisterationPlace { get; set; }
+
+        [MaxLength(20)]
+        public string CommercialNumber { get; set; }
+
+        public ICollection<PostalAddress> PostalAddressCollection { get; set; }
+
+        public ICollection<Communication> CommunicationCollection { get; set; }
+
+        #region IDateTracking
+
+        public DateTime createdDateTime { get; set; }
+
+        public DateTime? modifiedDateTime { get; set; }
+
+        #endregion IDateTracking
+
+        #region IUserTracking
+
+        public long creatorUserRefRecId { get; set; }
+
+        [ForeignKey(nameof(creatorUserRefRecId))]
+        public ApplicationUser creatorUser { get; set; }
+
+        public long? modifierUserRefRecId { get; set; }
+
+        [ForeignKey(nameof(modifierUserRefRecId))]
+        public ApplicationUser modifierUser { get; set; }
+
+        #endregion IUserTracking
+
+        #region Validation
+
+        public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrEmpty(Title))
+            {
+                yield return new ValidationResult(string.Format(Resource._0CanntBeEmpty, nameof(Title)), new[] { nameof(Title) });
+            }
+
+            if (string.IsNullOrEmpty(RegisterationNumber))
+            {
+                yield return new ValidationResult(string.Format(Resource._0CanntBeEmpty, nameof(RegisterationNumber)), new[] { nameof(RegisterationNumber) });
+            }
+        }
+
+        #endregion Validation
+    }
+}
