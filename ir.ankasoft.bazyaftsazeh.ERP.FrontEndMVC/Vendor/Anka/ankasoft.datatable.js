41fb39506555d915d18d89d9a0aff2c838a57960
Faraz.Loloei <FarazLoloei@gmail.com>
2017-10-14 09:48:00 +0330

    table header sorting icon toggled

--- ir.ankasoft.bazyaftsazeh.ERP.FrontEndMVC/Vendor/Anka/ankasoft.datatable.js
+++ ir.ankasoft.bazyaftsazeh.ERP.FrontEndMVC/Vendor/Anka/ankasoft.datatable.js
@@ -1,4 +1,11 @@
-﻿function managePagination(_currentPage) {
+﻿$(document).ready(function () {
+    $('table.anka_dataTable').find('thead tr th').each(function (e) {
+        var aTag = $(this).find('a');
+        aTag.bind('click', function () { toggleSortIcon(aTag); });
+    })
+});
+
+function managePagination(_currentPage) {
     var page = $('input[name=page]');
     var pageVal = page.val();
     switch (_currentPage) {
@@ -17,3 +24,12 @@
     page.val(pageVal);
     $('#dataTable').submit();
 };
+
+function toggleSortIcon(icon) {
+    var i = $(icon).parent().find('i');
+    if (i.hasClass('fa-sort')) {
+        i.removeClass('fa-sort');
+        i.addClass('fa-sort-amount-desc');
+    }
+    i.toggleClass('fa-sort-amount-desc fa-sort-amount-asc');
+}
