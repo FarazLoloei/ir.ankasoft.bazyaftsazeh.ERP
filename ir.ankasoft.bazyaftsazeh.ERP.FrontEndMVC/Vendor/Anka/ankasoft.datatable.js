83c1ef095cf2167ed497ea90f277e0e0c06b6621
Faraz.Loloei <FarazLoloei@gmail.com>
2017-10-15 09:35:55 +0330

    Datatable Sort completed

--- ir.ankasoft.bazyaftsazeh.ERP.FrontEndMVC/Vendor/Anka/ankasoft.datatable.js
+++ ir.ankasoft.bazyaftsazeh.ERP.FrontEndMVC/Vendor/Anka/ankasoft.datatable.js
@@ -1,9 +1,17 @@
 ﻿$(document).ready(function () {
+    //$('table.anka_dataTable').find('thead tr th').each(function (e) {
+    //    var aTag = $(this).find('a');
+    //    aTag.bind('click', function () { toggleSortIcon(aTag); });
+    //})
+    bindHeaderSortClick();
+});
+
+function bindHeaderSortClick() {
     $('table.anka_dataTable').find('thead tr th').each(function (e) {
         var aTag = $(this).find('a');
         aTag.bind('click', function () { toggleSortIcon(aTag); });
     })
-});
+}
 
 function managePagination(_currentPage) {
     var page = $('input[name=page]');
@@ -28,19 +36,28 @@ function managePagination(_currentPage) {
 function toggleSortIcon(header) {
     var $header = $(header);
     var i = $header.parent().find('i');
+    var thead = $('table.anka_dataTable').find('thead');
+    //console.info(3);
+    if ($header.data('column') != $('#sortBy').val())
+    {
+        var $i = thead.find('i');
+        $i.addClass('fa-sort');
+        $i.removeClass("fa-sort-amount-desc ").removeClass("fa-sort-amount-asc");
+    }
+    $('#sortBy').val($header.data('column'));
+    //console.info(1);
     if (i.hasClass('fa-sort')) {
         i.removeClass('fa-sort');
         i.addClass('fa-sort-amount-desc');
     }
     i.toggleClass('fa-sort-amount-desc fa-sort-amount-asc');
-
-    var thead = $('table.anka_dataTable').find('thead');
-    console.info(thead);
-    $('#sortBy').val($header.data('column'));
+    //console.info(2);
+    
     var sortType = 1;
     if (i.hasClass('fa-sort-amount-desc'))
         sortType = 2;
+    //console.info(4);
     $('#sortType').val(sortType);
-
+    //console.info(5);
     $('#dataTable').submit();
 }
