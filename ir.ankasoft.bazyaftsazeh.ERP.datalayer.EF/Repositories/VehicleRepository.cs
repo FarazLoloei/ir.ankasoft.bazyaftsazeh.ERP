28413d9bbe30eb4e46587a7080575c6919a516e5
FarazLoloei <farazloloei@gmail.com>
2017-11-03 15:30:22 +0330

    VehicleRepository Added But not completed

---
+++ ir.ankasoft.bazyaftsazeh.ERP.datalayer.EF/Repositories/VehicleRepository.cs
@@ -0,0 +1,32 @@
+﻿using ir.ankasoft.bazyaftsazeh.ERP.entities;
+using ir.ankasoft.bazyaftsazeh.ERP.entities.Repositories;
+using ir.ankasoft.infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ir.ankasoft.bazyaftsazeh.ERP.datalayer.EF.Repositories
+{
+    public class VehicleRepository : Repository<Vehicle>, IVehicleRepository
+    {
+        public override IEnumerable<Vehicle> LoadByFilter(IFilterDataSource request,
+                                           out int totalRecords)
+        {
+            throw new NotImplementedException();
+            IQueryable<Vehicle> objects = FindAll(
+                //x => x.Name.Contains(request.keyword) ||
+                //                                      x.Family.Contains(request.keyword), y => y.Party
+                                                     ).AsQueryable();
+            totalRecords = objects.Count();
+            return objects.OrderBy(BuildOrderBy(request.sort.Key, request.sort.Value.ToString())).Skip((request.page * request.pageSize) - request.pageSize).Take(request.pageSize);
+        }
+
+        private string BuildOrderBy(string sortOn, string sortDirection)
+        {
+            return string.Format("{0} {1}", sortOn, sortDirection);
+        }
+    }
+}
---
+++ ir.ankasoft.bazyaftsazeh.ERP.entities/Repositories/IVehicleRepository.cs
@@ -0,0 +1,11 @@
+﻿using ir.ankasoft.infrastructure;
+using System.Collections.Generic;
+
+namespace ir.ankasoft.bazyaftsazeh.ERP.entities.Repositories
+{
+    public interface IVehicleRepository : IRepository<Vehicle, long>
+    {
+        new IEnumerable<Vehicle> LoadByFilter(IFilterDataSource request,
+                                              out int totalRecords);
+    }
+}
