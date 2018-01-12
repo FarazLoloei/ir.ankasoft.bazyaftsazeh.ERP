86f0ffa6e7dd6e35815b0e0fa20ea3969f0d83a0
Faraz.lOlOEI <FarazLoloei@live.com>
2018-01-12 23:16:50 +0330

    DocumentStatus Objects

---
+++ ir.ankasoft.bazyaftsazeh.ERP.entities/DocumentStatus.cs
@@ -0,0 +1,61 @@
+﻿using ir.ankasoft.entities;
+using ir.ankasoft.infrastructure;
+using ir.ankasoft.resource;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ir.ankasoft.bazyaftsazeh.ERP.entities
+{
+    public class DocumentStatus : DomainEntity<long>, IDateTracking, IUserTracking
+    {
+
+        public long DocumentRefRecId { get; set; }
+
+        [ForeignKey(nameof(DocumentRefRecId))]
+        public Document Document { get; set; }
+
+        public string Description { get; set; }
+
+
+
+        #region IDateTracking
+
+        public DateTime createdDateTime { get; set; }
+
+        public DateTime? modifiedDateTime { get; set; }
+
+        #endregion IDateTracking
+
+        #region IUserTracking
+
+        public long creatorUserRefRecId { get; set; }
+
+        [ForeignKey(nameof(creatorUserRefRecId))]
+        public ApplicationUser creatorUser { get; set; }
+
+        public long? modifierUserRefRecId { get; set; }
+
+        [ForeignKey(nameof(modifierUserRefRecId))]
+        public ApplicationUser modifierUser { get; set; }
+
+        #endregion IUserTracking
+
+        #region Validation
+
+        public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (DocumentRefRecId < 1)
+                yield return new ValidationResult(
+                    string.Format(Resource._0CanntBeEmpty,
+                                  nameof(DocumentRefRecId)),
+                    new[] { nameof(DocumentRefRecId) });
+        }
+
+        #endregion Validation
+    }
+}
